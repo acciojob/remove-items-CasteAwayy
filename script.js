47c9d3864c3eece8b998d42ff67786920278e084
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-01 00:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const button = document.querySelector('input[type=button]');
+const select = document.querySelector('#colorSelect');
+
+button.addEventListener('click' , ()=>{
+
+    var arr=  select.options;
+    var target = select.value;
+    for(let i of arr)  if(i.value == target){i.remove();}
+});
